2e50d509050a3adc1de8b0565c7043cd4960ec73
ahwong5 <ahwong5@gmail.com>
2016-09-13 00:54:19 +0800

    validate User and Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,8 @@
 class Listing < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates_presence_of :image
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
 end
